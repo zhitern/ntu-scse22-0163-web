d23376fee1246d3cf69fcef9950caa7f524783af
zhitern <tangzhitern@gmail.com>
2022-08-30 12:47:07 +0800

    First Push
    project base

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <body>Hello World2
+        <button>TESTY</button>
+      </body>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/MainPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <body>Main Page
+      </body>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
