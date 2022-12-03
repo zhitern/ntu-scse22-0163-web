5d352d0c7b56cb5cf8a3e5bb4fa32700701d1888
Chewposhi <chewposhi@gmail.com>
2022-12-04 03:04:23 +0800

    added banner
    1. added banner text, image, title

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -3,15 +3,13 @@ import React from 'react';
 const Navbar = () => {
     return ( 
         <nav className="navbar">
-            <h1>Header</h1>
             <div className="links">
                 <a href="/Home">Home</a>
                 <a href="/GeoTopicModeling">Geo Topic Modeling</a>
                 <a href="/RegionSearch">Region Search</a>
                 <a href="/RegionTopicExploration">Region Topic Exploration</a>
                 <a href="/DeepRegionRepresentation">Deep Region Representation</a>
-                <a href="/RelatedReseachProjects">Related Reseach Projects</a>
-
+                <a href="https://personal.ntu.edu.sg/gaocong/projectS.html" target="_blank">Related Reseach Projects</a>
             </div>
         </nav>
     );
--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
 import Navbar from '../Components/Navbar'
+import banner from '../image/banner.jpg'
 
 function MainPage() {
   return (
-    <div className="MainPage">
+    <div>
+      <h1 className="Title">Title/logo</h1>
       <Navbar/>
-      <header className="MainPage-header">
-      <body>Main Page
-      </body>
-      
-      </header>
+      <div className="bannerText">Explore Geospatial data analytics</div>
+      <img src={banner} className = "bannerImage" alt="" />
+      <p className="bannerSubText">A portal to 4 geospatial data analytic tools</p>
     </div>
   );
 }
