ab4a1d7e113b1711c18c66727cd671a768b4f42f
Chewposhi <chewposhi@gmail.com>
2022-12-04 15:26:18 +0800

    checkpoint before trying to solve browser resize issue

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -5,12 +5,25 @@ import banner from '../image/banner.jpg'
 
 function MainPage() {
   return (
-    <div>
-      <h1 className="Title">Title/logo</h1>
-      <Navbar/>
-      <div className="bannerText">Explore Geospatial data analytics</div>
-      <img src={banner} className = "bannerImage" alt="" />
-      <p className="bannerSubText">A portal to 4 geospatial data analytic tools</p>
+    <div className='flexContainer'>
+      
+      <div className="Top">
+        <h1 className="Title">Title/logo</h1>
+        <Navbar/>
+      </div>
+      
+      <div className="Banner">
+        <p className="bannerText">Explore Geospatial data analytics</p>
+        <img src={banner} className = "bannerImage" alt="" />
+        <p className="bannerSubText">A portal to 4 geospatial data analytic tools</p>
+      </div>
+      
+      
+      {/* region search */}
+      <div className='regionSearch'>
+        <button className='regionSearchBtn'>Click for Region Search</button>
+      </div>
+      
     </div>
   );
 }
