e004d58f6a3825076935620eb22821ce8112bd3c
zhitern <tangzhitern@gmail.com>
2022-12-05 13:35:50 +0800

    added css file for algoshowcase page

--- src/Pages/AlgoShowcasePage.tsx
+++ src/Pages/AlgoShowcasePage.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import './AlgoShowcasePage.css';
 
 function AlgoShowcasePage() {
   return (
     <div className="AlgoShowcasePage">
       <header className="AlgoShowcasePage-header">
-      <body>Algo Page
-      </body>
-      
       </header>
+      <body>
+        <div>
+          <h1 style={{display: "inline"}}>LOGO/TITLE</h1>
+          <div style={{display: "inline"}}>NAV BAR3</div>
+        </div>
+        <div className="InputBox">INPUT BOX</div>
+        <div className="MapBoundary">MAP ZONE</div>
+      </body>
     </div>
   );
 }
