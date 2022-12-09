e42a31c63c83e4e65fe2c28751729d966e190969
zhitern <tangzhitern@gmail.com>
2022-12-09 12:17:30 +0800

    updated package and packagelock

--- src/Pages/AlgoShowcasePage.tsx
+++ src/Pages/AlgoShowcasePage.tsx
@@ -13,14 +13,14 @@ function AlgoShowcasePage() {
       <div className="Top">
         <h1 className="Title">Title/logo</h1>
         <Navbar/>
-      </div>      
-      <div style={{display: 'flex', paddingTop: '5%', height: '80vh', justifyContent: 'space-evenly'}}>
+      </div>
+      <div style={{display: 'flex', paddingTop: '5%', height: '70vh', justifyContent: 'space-evenly'}}>
         <div className="InputBox" style={{background: 'red', display: 'inline-block'}}>
           INPUT BOX
           <div>test 1</div>
           <div>test 2</div>
         </div>
-        <MapContainer style={{width: '65%', display: 'inline-block'}} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <MapContainer style={{height: 'match-parent', width: '65%', display: 'inline-block'}} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
