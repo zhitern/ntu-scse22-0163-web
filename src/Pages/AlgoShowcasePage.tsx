f7e7e4f185599b8b1ce10289984db57647983ed4
Chewposhi <chewposhi@gmail.com>
2022-12-13 23:38:45 +0800

    Update AlgoShowcasePage.tsx

--- src/Pages/AlgoShowcasePage.tsx
+++ src/Pages/AlgoShowcasePage.tsx
@@ -77,7 +77,7 @@ function AlgoShowcasePage() {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-
+          <LocateSelf/>
         </MapContainer>
 
       </div>
