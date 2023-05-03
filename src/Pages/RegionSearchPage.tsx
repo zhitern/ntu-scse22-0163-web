c1b5a4c8f8309bd8caba2547dcd3f546c9e7c7ec
Chew Poshi <chewposhi@gmail.com>
2023-05-03 19:55:19 +0800

    changed components back to Components

--- src/Pages/RegionSearchPage.tsx
+++ src/Pages/RegionSearchPage.tsx
@@ -2,8 +2,8 @@ import { LatLngBoundsExpression } from 'leaflet';
 import React from 'react';
 import { useState } from 'react';
 import { Rectangle } from 'react-leaflet';
-import Map from '../components/Map';
-import InputForm from '../components/InputForm';
+import Map from '../Components/Map';
+import InputForm from '../Components/InputForm';
 
 function RegionSearchPage() {
   interface MapShape {
